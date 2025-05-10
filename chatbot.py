53ebd707586c3465746f883a72f75593fd0dd095
josmyrose <josmyrose@gmail.com>
2025-05-10 10:45:15 +0200

    Files updated

--- chatbot.py
+++ chatbot.py
@@ -1,8 +1,15 @@
 
 import openai
 from openai import OpenAI
+import os
 
-client = OpenAI()
+#client = OpenAI()
+
+api_key = os.getenv("OPENAI_API_KEY")
+if not api_key:
+    raise ValueError("OPENAI_API_KEY not set in environment.")
+
+client = OpenAI(api_key=api_key)
 
 def llm_response(user_input):
     response = client.chat.completions.create(
